9e597ce0492425438b3b616fcbad48e6580495ec
shanghaitao <359310444@qq.com>
2018-03-15 11:11:55 +0800

    up ts css

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,6 +6,14 @@ declare var process: {
     }
 }
 
+declare module '*.css' {
+    interface IClassNames {
+        [className: string]: string
+    }
+    const classNames: IClassNames
+    export = classNames
+}
+
 declare interface PlainObject {
     [propName: string]: any
 }
