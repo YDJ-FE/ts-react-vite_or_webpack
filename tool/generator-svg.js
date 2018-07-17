9b46a5bd37bf67bc549150ac0abfbb4b741b69fd
shanghaitao <359310444@qq.com>
2018-07-17 10:36:16 +0800

    up rm gen svg

--- tool/generator-svg.js
+++
@@ -1,8 +0,0 @@
-const generator = require('typescript-react-svg-icon-generator')
-
-const config = {
-    svgDir: './src/assets/svg/',
-    destination: './src/components/Icons/index.tsx',
-    tslintDisable: false
-}
-generator(config)
