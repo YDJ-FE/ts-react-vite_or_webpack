a7d57bca1ca724b9245e1c90e6bc7ac8be3ffefe
shanghaitao <359310444@qq.com>
2018-07-23 10:56:32 +0800

    up assetsPublicPath

--- build/config.js
+++ build/config.js
@@ -8,7 +8,7 @@ const STATICDOMAIN = constants.APP_ENV === 'prod' ? '.' : ''
 module.exports = {
     index: path.resolve(__dirname, `./../dist/${constants.APP_ENV}/index.html`),
     assetsRoot: path.resolve(__dirname, `./../dist/${constants.APP_ENV}`),
-    assetsPublicPath: constants.APP_ENV === 'dev' ? '/' : `${STATICDOMAIN}/dist/${constants.APP_ENV}/`,
+    assetsPublicPath: constants.APP_ENV === 'dev' ? '/' : `${STATICDOMAIN}/`,
     assetsSubDirectory: 'static',
     // 正式环境接入sentry需要sourceMap
     sourceMap: constants.APP_ENV !== 'qa',
