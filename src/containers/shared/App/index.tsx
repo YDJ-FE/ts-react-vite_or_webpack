3769b49509e9067a46c9b7683aec232529ce9f56
dachunchun <524112857@qq.com>
2018-08-01 14:47:24 +0800

    up

--- src/containers/shared/App/index.tsx
+++ src/containers/shared/App/index.tsx
@@ -1,24 +1,53 @@
+
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
+import Loadable from 'react-loadable'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import * as styles from './index.scss'
+import PageLoading from '@components/PageLoading'
 import Error from '@components/Error'
 
-import { AuthorizedRoute } from 'react-authorized'
-import { normalRoutes } from './routeConfig'
+const Home = Loadable({
+    loader: () => import(/* webpackChunkName: "home" */ '@views/Home'),
+    loading: PageLoading
+})
+const Login = Loadable({
+    loader: () => import(/* webpackChunkName: "login" */ '@views/Login'),
+    loading: PageLoading
+})
+
+// 权限控制
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route
+        {...rest}
+        render={props =>
+            true ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: '/login',
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
+)
 
 const AppWrapper = props => <div className={styles.appWrapper}>{props.children}</div>
 
-class AppRouter extends React.Component {
-    render() {
-        return (
-            <AuthorizedRoute
-                normalRoutes={normalRoutes}
-                normalLayout={AppWrapper}
-                notFound={Error}
-            />
-        )
-    }
-}
+const AppRouter = () => (
+    <AppWrapper>
+        <Router>
+            <Switch>
+                <Route exact path="/login" component={Login} />
+                <PrivateRoute path="/" component={Home} />
+                <Route component={Error} />
+            </Switch>
+        </Router>
+    </AppWrapper>
+)
 
 export default hot(module)(AppRouter)
