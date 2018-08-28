0dcea94aab8491709c90fbc70e48d0a6ec471fae
dachunchun <524112857@qq.com>
2018-08-28 16:04:13 +0800

    up 配置jest+enzyme单元测试，添加测试用例

---
+++ __test__/login.test.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { shallow, configure } from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16'
+
+import Login from '../src/containers/views/Login'
+
+configure({ adapter: new Adapter() })
+
+const setup = () => {
+    // 通过 enzyme 提供的 shallow(浅渲染) 创建组件
+    const wrapper = shallow(<Login />)
+    return {
+        wrapper
+    }
+}
+
+describe('Login', () => {
+    const { wrapper } = setup()
+    it('Login component should be render', () => {
+        expect(wrapper.find('input').exists())
+    })
+})
