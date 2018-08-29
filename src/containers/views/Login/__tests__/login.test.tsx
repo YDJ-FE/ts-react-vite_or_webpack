bd8d497c0e3c62f7162f0e27a16f887f9824001b
dachunchun <524112857@qq.com>
2018-08-29 09:33:05 +0800

    up 调整测试目录位置

--- src/containers/views/Login/__tests__/login.test.tsx
+++ src/containers/views/Login/__tests__/login.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { shallow, configure } from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
 
-import Login from '../src/containers/views/Login'
+import Login from './../index'
 
 configure({ adapter: new Adapter() })
 
