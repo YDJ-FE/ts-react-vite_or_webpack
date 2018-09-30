008cc3a0f4dd357e8898184c624472975cc5938c
shanghaitao <359310444@qq.com>
2018-09-30 13:08:04 +0800

    up stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,9 @@
 // @ts-nocheck
 module.exports = {
-    ignoreFiles: [
-        'node_modules/**/*.scss',
-        '**/*.md',
-        '**/*.ts',
-        '**/*.tsx',
-        '**/*.js'
-    ],
-    extends: [
-        'stylelint-config-standard',
-        'stylelint-config-ydj/scss',
-        'stylelint-config-css-modules'
-    ]
+    ignoreFiles: ['node_modules/**/*.scss', '**/*.md', '**/*.ts', '**/*.tsx', '**/*.js'],
+    extends: ['stylelint-config-css-modules', 'stylelint-config-standard', 'stylelint-config-ydj/scss'],
+    rules: {
+        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'local'] }],
+        'property-no-unknown': [true, { ignoreProperties: ['composes'] }]
+    }
 }
