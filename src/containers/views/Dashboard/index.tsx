e839a4355168d1a5ec68f25527a97183bfe0f341
shanghaitao <359310444@qq.com>
2019-02-10 18:09:44 +0800

    simply use hooks

--- src/containers/views/Dashboard/index.tsx
+++ src/containers/views/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react'
+import { Avatar, Button } from 'antd'
 
 import * as styles from './style.scss'
 import ChartAreaStack from '@shared/ChartAreaStack'
 
+const AVATAR_URL = 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+
 function Dashboard() {
+    const [avatars, setAvatars] = React.useState([AVATAR_URL])
+    function addAvatar() {
+        const newAvatars = [...avatars, `${AVATAR_URL}?t=${Date.now()}`]
+        setAvatars(newAvatars)
+    }
+    React.useEffect(() => {
+        if (avatars.length > 1) {
+            document.title = `${avatars.length} avatars!`
+        }
+    }, [avatars])
     return (
         <div className={styles.dashboard}>
+            <div className={styles.avatarsContainer}>
+                <Button type="primary" onClick={addAvatar}>
+                    add
+                </Button>
+                <div className={styles.avatars}>
+                    {avatars.map(a => (
+                        <Avatar style={{ marginLeft: 10 }} key={a} src={a} />
+                    ))}
+                </div>
+            </div>
             <ChartAreaStack style={{ height: 600, margin: '20px 0' }} />
         </div>
     )
--- src/containers/views/Dashboard/style.scss.d.ts
+++ src/containers/views/Dashboard/style.scss.d.ts
@@ -1,1 +1,3 @@
 export const dashboard: string;
+export const avatarsContainer: string;
+export const avatars: string;
