dac3809b2886a724d311508b20e3b3bd353fe6b9
shanghaitao <359310444@qq.com>
2019-02-11 14:14:59 +0800

    new way to use table

--- src/containers/views/Users/Table/index.tsx
+++ src/containers/views/Users/Table/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Table, Divider, Popconfirm } from 'antd'
-import { ColumnProps } from 'antd/lib/table'
 import { PaginationConfig } from 'antd/lib/pagination'
 import { inject, observer } from 'mobx-react'
 import { observable, action } from 'mobx'
@@ -22,26 +21,6 @@ interface IStoreProps {
 interface IProps extends IStoreProps {
     scrollY: number
 }
-
-const baseColumns: Array<ColumnProps<IUserStore.IUser>> = [
-    {
-        title: 'Account',
-        dataIndex: 'account',
-        key: 'account'
-    },
-    {
-        title: 'Category',
-        dataIndex: 'category',
-        key: 'category'
-    },
-    {
-        title: 'CreatedAt',
-        dataIndex: 'createdAt',
-        key: 'createdAt'
-    }
-]
-
-class TableExtended extends Table<IUserStore.IUser> {}
 
 @inject(
     (store: IStore): IStoreProps => {
@@ -92,38 +71,14 @@ class UserTable extends ComponentExt<IProps> {
             pageSize,
             total
         } = this.props
-        const columns = baseColumns.concat([
-            {
-                title: 'Action',
-                key: 'action',
-                render: (_, record) => (
-                    <span>
-                        <a href="javascript:;" onClick={() => this.modifyUser(record)}>
-                            Modify
-                        </a>
-                        <Divider type="vertical" />
-                        <Popconfirm
-                            placement="top"
-                            title="确认删除?"
-                            onConfirm={() => deleteUser(record._id)}
-                            okText="Yes"
-                            cancelText="No"
-                        >
-                            <a href="javascript:;">Delete</a>
-                        </Popconfirm>
-                    </span>
-                )
-            }
-        ])
         return (
             <React.Fragment>
-                <TableExtended
+                <Table<IUserStore.IUser>
                     className="center-table"
                     style={{ width: '100%' }}
                     bordered
                     rowKey="_id"
                     loading={getUsersloading}
-                    columns={columns}
                     dataSource={users}
                     scroll={{ y: scrollY }}
                     pagination={{
@@ -134,7 +89,38 @@ class UserTable extends ComponentExt<IProps> {
                         total
                     }}
                     onChange={handleTableChange}
-                />
+                >
+                    <Table.Column<IUserStore.IUser> key="account" title="Account" dataIndex="account" width={200} />
+                    <Table.Column<IUserStore.IUser> key="category" title="Category" dataIndex="category" width={100} />
+                    <Table.Column<IUserStore.IUser>
+                        key="createdAt"
+                        title="CreatedAt"
+                        dataIndex="createdAt"
+                        width={200}
+                    />
+                    <Table.Column<IUserStore.IUser>
+                        key="action"
+                        title="Action"
+                        width={120}
+                        render={(_, record) => (
+                            <span>
+                                <a href="javascript:;" onClick={() => this.modifyUser(record)}>
+                                    Modify
+                                </a>
+                                <Divider type="vertical" />
+                                <Popconfirm
+                                    placement="top"
+                                    title="确认删除?"
+                                    onConfirm={() => deleteUser(record._id)}
+                                    okText="Yes"
+                                    cancelText="No"
+                                >
+                                    <a href="javascript:;">Delete</a>
+                                </Popconfirm>
+                            </span>
+                        )}
+                    />
+                </Table>
                 <UserModal
                     visible={this.userModalVisible}
                     onCancel={this.hideUserModalVisible}
