36884651ea4f228bb9dc95db2f9a7036f9c479a8
shanghaitao <359310444@qq.com>
2019-02-11 14:52:45 +0800

    test assets

---
+++ typings/assets.d.ts
@@ -0,0 +1,22 @@
+declare interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+declare module '*.svg' {
+    const content: SvgrComponent
+    export default content
+}
+
+declare module '*.png' {
+    const content: any
+    export default content
+}
+declare module '*.jpg' {
+    const content: any
+    export default content
+}
+declare module '*.jpeg' {
+    const content: any
+    export default content
+}
+declare module '*.gif' {
+    const content: any
+    export default content
+}
--- typings/svg.d.ts
+++
@@ -1,6 +0,0 @@
-declare interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
-
-declare module '*.svg' {
-    const content: SvgrComponent
-    export default content
-}
