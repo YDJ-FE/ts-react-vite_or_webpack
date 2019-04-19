cdffede4d3142eaeccf49a1f8d0f6d664cf85fe0
shanghaitao <359310444@qq.com>
2019-04-19 19:19:46 +0800

    refactor: transform Sider component from class to function

--- src/containers/views/Home/Sider/index.tsx
+++ src/containers/views/Home/Sider/index.tsx
@@ -10,56 +10,45 @@ interface IStoreProps {
     sideBarCollapsed?: boolean
     sideBarTheme?: IGlobalStore.SideBarTheme
     changeSiderTheme?: (theme: IGlobalStore.SideBarTheme) => void
-    routerStore?: RouterStore
 }
 
-@inject(
+function Sider({ sideBarCollapsed, sideBarTheme, changeSiderTheme }: IStoreProps) {
+    const ChangeTheme = (
+        <div className={classnames(styles.changeTheme, sideBarTheme === 'dark' && styles.dark)}>
+            Switch Theme
+            <Switch
+                checkedChildren="dark"
+                unCheckedChildren="light"
+                checked={sideBarTheme === 'dark'}
+                onChange={val => changeSiderTheme(val ? 'dark' : 'light')}
+            />
+        </div>
+    )
+    return (
+        <Layout.Sider
+            className={styles.sider}
+            trigger={null}
+            theme={sideBarTheme}
+            collapsible
+            collapsed={sideBarCollapsed}
+        >
+            <div className={classnames(styles.logoBox, sideBarTheme === 'dark' && styles.dark)}>
+                <Icon type="ant-design" />
+            </div>
+            <SiderMenu />
+            {!sideBarCollapsed && ChangeTheme}
+        </Layout.Sider>
+    )
+}
+
+export default inject(
     (store: IStore): IStoreProps => {
-        const { routerStore, globalStore } = store
+        const { globalStore } = store
         const { sideBarCollapsed, sideBarTheme, changeSiderTheme } = globalStore
         return {
-            routerStore,
             sideBarCollapsed,
             sideBarTheme,
             changeSiderTheme
         }
     }
-)
-@observer
-class Sider extends React.Component<IStoreProps> {
-    handleThemeChange = (e: boolean) => {
-        this.props.changeSiderTheme(e ? 'dark' : 'light')
-    }
-
-    render() {
-        const { sideBarCollapsed, sideBarTheme } = this.props
-        const ChangeTheme = (
-            <div className={classnames(styles.changeTheme, sideBarTheme === 'dark' && styles.dark)}>
-                Switch Theme
-                <Switch
-                    checkedChildren="dark"
-                    unCheckedChildren="light"
-                    checked={sideBarTheme === 'dark'}
-                    onChange={this.handleThemeChange}
-                />
-            </div>
-        )
-        return (
-            <Layout.Sider
-                className={styles.sider}
-                trigger={null}
-                theme={sideBarTheme}
-                collapsible
-                collapsed={sideBarCollapsed}
-            >
-                <div className={classnames(styles.logoBox, sideBarTheme === 'dark' && styles.dark)}>
-                    <Icon type="ant-design" />
-                </div>
-                <SiderMenu />
-                {!sideBarCollapsed && ChangeTheme}
-            </Layout.Sider>
-        )
-    }
-}
-
-export default Sider
+)(observer(Sider))
