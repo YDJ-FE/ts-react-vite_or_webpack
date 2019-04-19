0c6a4345827e859682cc7ae5f27f5e64546cf49e
shanghaitao <359310444@qq.com>
2019-04-19 17:41:11 +0800

    refactor: transform Login component from class to function

--- src/containers/views/Login/index.tsx
+++ src/containers/views/Login/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { inject, observer } from 'mobx-react'
-import { observable, runInAction } from 'mobx'
+import { inject } from 'mobx-react'
+import { observer } from 'mobx-react-lite'
 import { Form, Icon, Input, Button } from 'antd'
 import { FormComponentProps } from 'antd/lib/form'
 import { hot } from 'react-hot-loader'
@@ -10,92 +10,78 @@ import * as styles from './index.scss'
 const FormItem = Form.Item
 
 interface IStoreProps {
-    routerStore?: RouterStore
     login?: (data: IAuthStore.LoginParams) => Promise<any>
 }
 
-@inject(
-    (store: IStore): IStoreProps => {
-        const { routerStore, authStore } = store
-        const { login } = authStore
-        return {
-            routerStore,
-            login
-        }
-    }
-)
-@observer
-class Login extends React.Component<IStoreProps & FormComponentProps> {
-    @observable
-    private loading: boolean = false
+function Login({ login, form }: IStoreProps & FormComponentProps) {
+    const [loading, setLoading] = React.useState(false)
 
-    submit = (e: React.FormEvent<any>): void => {
+    const submit = (e: React.FormEvent<any>): void => {
+        // test.status = true
         e.preventDefault()
-        this.props.form.validateFields(
+        form.validateFields(
             async (err, values): Promise<any> => {
                 if (!err) {
-                    runInAction('SHOW_LOGIN_LOADING', () => {
-                        this.loading = true
-                    })
-                    await this.props.login(values)
-                    runInAction('HIDE_LOGIN_LOADING', () => {
-                        this.loading = false
-                    })
+                    setLoading(true)
+                    try {
+                        await login(values)
+                    } catch (err) {}
+                    setLoading(false)
                 }
             }
         )
     }
 
-    render() {
-        const { getFieldDecorator } = this.props.form
-        return (
-            <div className={styles.login}>
-                <Form onSubmit={this.submit} className={styles.form}>
-                    <div className={styles.logoBox}>
-                        <Icon type="ant-design" />
+    const { getFieldDecorator } = form
+    return (
+        <div className={styles.login}>
+            <Form onSubmit={submit} className={styles.form}>
+                <div className={styles.logoBox}>
+                    <Icon type="ant-design" />
+                </div>
+                <FormItem hasFeedback>
+                    {getFieldDecorator('account', {
+                        rules: [{ required: true }]
+                    })(
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="account"
+                        />
+                    )}
+                </FormItem>
+                <FormItem hasFeedback>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true }]
+                    })(
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="password"
+                        />
+                    )}
+                </FormItem>
+                <FormItem>
+                    <div className={styles.tips}>
+                        <span>username: admin</span>
+                        <span>password: admin</span>
                     </div>
-                    <FormItem hasFeedback>
-                        {getFieldDecorator('account', {
-                            rules: [
-                                {
-                                    required: true
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="account"
-                            />
-                        )}
-                    </FormItem>
-                    <FormItem hasFeedback>
-                        {getFieldDecorator('password', {
-                            rules: [
-                                {
-                                    required: true
-                                }
-                            ]
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="password"
-                            />
-                        )}
-                    </FormItem>
-                    <FormItem>
-                        <div className={styles.tips}>
-                            <span>username: admin</span>
-                            <span>password: admin</span>
-                        </div>
-                        <Button type="primary" htmlType="submit" block loading={this.loading}>
-                            login
-                        </Button>
-                    </FormItem>
-                </Form>
-            </div>
-        )
-    }
+                    <Button type="primary" htmlType="submit" block loading={loading}>
+                        login
+                    </Button>
+                </FormItem>
+            </Form>
+        </div>
+    )
 }
 
-export default hot(module)(Form.create<{}>()(Login))
+export default hot(module)(
+    Form.create<{}>()(
+        inject(
+            (store: IStore): IStoreProps => {
+                const { authStore } = store
+                const { login } = authStore
+                return { login }
+            }
+        )(observer(Login))
+    )
+)
