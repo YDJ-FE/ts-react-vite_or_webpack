d490a815d2701f593dad1851e3e07609788add2b
shanghaitao <359310444@qq.com>
2019-05-08 19:36:53 +0800

    chore: types

--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -1,8 +1,7 @@
 import http from '@services/http'
 
 export default {
-    // 登录
-    login(data): Promise<any> {
+    login(data: object): Promise<any> {
         return http.post('auth/login', data || {})
     }
 }
--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -1,19 +1,19 @@
 import http from '@services/http'
 
 export default {
-    getUsers(data): Promise<any> {
+    getUsers(data: object): Promise<any> {
         return http.get('user', data || {})
     },
 
-    createUser(data): Promise<any> {
+    createUser(data: object): Promise<any> {
         return http.post('user/create', data || {})
     },
 
-    modifyUser(data): Promise<any> {
+    modifyUser(data: object): Promise<any> {
         return http.post('user/modify', data || {})
     },
 
-    deleteUser(data): Promise<any> {
+    deleteUser(data: object): Promise<any> {
         return http.post('user/delete', data || {})
     }
 }
