72bd4e5775e7d09f9a6e7c8c7e8e8ee57faab27f
shanghaitao <359310444@qq.com>
2019-05-10 23:00:45 +0800

    chore: functional UserTable

--- src/containers/views/Users/Table/index.tsx
+++ src/containers/views/Users/Table/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
 import { Table, Divider, Popconfirm } from 'antd'
 import { PaginationConfig } from 'antd/lib/pagination'
-import { inject, observer } from 'mobx-react'
-import { observable, action } from 'mobx'
+import { inject } from 'mobx-react'
+import { observer } from 'mobx-react-lite'
 
-import { ComponentExt } from '@utils/reactExt'
 import UserModal from './../UserModal'
 
 interface IStoreProps {
@@ -22,7 +21,85 @@ interface IProps extends IStoreProps {
     scrollY: number
 }
 
-@inject(
+function UserTable({
+    scrollY,
+    getUsersloading,
+    users,
+    getUsers,
+    deleteUser,
+    handleTableChange,
+    pageIndex,
+    pageSize,
+    total
+}: IProps) {
+    const useOnMount = (onMount: () => void) =>
+        React.useEffect(() => {
+            if (onMount) {
+                onMount()
+            }
+        }, [])
+
+    useOnMount(getUsers)
+
+    const [modalVisible, setModalVisible] = React.useState(false)
+    const [currentUser, setCurrentUser] = React.useState<IUserStore.IUser>(null)
+
+    function modifyUser(user: IUserStore.IUser) {
+        setCurrentUser(user)
+        setModalVisible(true)
+    }
+
+    return (
+        <React.Fragment>
+            <Table<IUserStore.IUser>
+                className="center-table"
+                style={{ width: '100%' }}
+                bordered
+                rowKey="_id"
+                loading={getUsersloading}
+                dataSource={users}
+                scroll={{ y: scrollY }}
+                pagination={{
+                    current: pageIndex,
+                    showSizeChanger: true,
+                    pageSize,
+                    pageSizeOptions: ['30', '20', '10'],
+                    total
+                }}
+                onChange={handleTableChange}
+            >
+                <Table.Column<IUserStore.IUser> key="account" title="Account" dataIndex="account" width={200} />
+                <Table.Column<IUserStore.IUser> key="category" title="Category" dataIndex="category" width={100} />
+                <Table.Column<IUserStore.IUser> key="createdAt" title="CreatedAt" dataIndex="createdAt" width={200} />
+                <Table.Column<IUserStore.IUser>
+                    key="action"
+                    title="Action"
+                    width={120}
+                    render={(_, record) => (
+                        <span>
+                            <a href="javascript:;" onClick={() => modifyUser(record)}>
+                                Modify
+                            </a>
+                            <Divider type="vertical" />
+                            <Popconfirm
+                                placement="top"
+                                title="确认删除?"
+                                onConfirm={() => deleteUser(record._id)}
+                                okText="Yes"
+                                cancelText="No"
+                            >
+                                <a href="javascript:;">Delete</a>
+                            </Popconfirm>
+                        </span>
+                    )}
+                />
+            </Table>
+            <UserModal visible={modalVisible} onCancel={() => setModalVisible(false)} user={currentUser} />
+        </React.Fragment>
+    )
+}
+
+export default inject(
     (store: IStore): IStoreProps => {
         const {
             getUsersloading,
@@ -36,99 +113,4 @@ interface IProps extends IStoreProps {
         } = store.userStore
         return { getUsersloading, users, getUsers, deleteUser, handleTableChange, pageIndex, pageSize, total }
     }
-)
-@observer
-class UserTable extends ComponentExt<IProps> {
-    @observable
-    private userModalVisible: boolean = false
-
-    @observable
-    private currentUser: IUserStore.IUser = null
-
-    @action
-    hideUserModalVisible = () => {
-        this.userModalVisible = !this.userModalVisible
-    }
-
-    @action
-    modifyUser = (user: IUserStore.IUser) => {
-        this.currentUser = user
-        this.userModalVisible = true
-    }
-
-    componentDidMount() {
-        this.props.getUsers()
-    }
-
-    render() {
-        const {
-            scrollY,
-            getUsersloading,
-            users,
-            deleteUser,
-            handleTableChange,
-            pageIndex,
-            pageSize,
-            total
-        } = this.props
-        return (
-            <React.Fragment>
-                <Table<IUserStore.IUser>
-                    className="center-table"
-                    style={{ width: '100%' }}
-                    bordered
-                    rowKey="_id"
-                    loading={getUsersloading}
-                    dataSource={users}
-                    scroll={{ y: scrollY }}
-                    pagination={{
-                        current: pageIndex,
-                        showSizeChanger: true,
-                        pageSize,
-                        pageSizeOptions: ['30', '20', '10'],
-                        total
-                    }}
-                    onChange={handleTableChange}
-                >
-                    <Table.Column<IUserStore.IUser> key="account" title="Account" dataIndex="account" width={200} />
-                    <Table.Column<IUserStore.IUser> key="category" title="Category" dataIndex="category" width={100} />
-                    <Table.Column<IUserStore.IUser>
-                        key="createdAt"
-                        title="CreatedAt"
-                        dataIndex="createdAt"
-                        width={200}
-                    />
-                    <Table.Column<IUserStore.IUser>
-                        key="action"
-                        title="Action"
-                        width={120}
-                        render={(_, record) => (
-                            <span>
-                                <a href="javascript:;" onClick={() => this.modifyUser(record)}>
-                                    Modify
-                                </a>
-                                <Divider type="vertical" />
-                                <Popconfirm
-                                    placement="top"
-                                    title="确认删除?"
-                                    onConfirm={() => deleteUser(record._id)}
-                                    okText="Yes"
-                                    cancelText="No"
-                                >
-                                    <a href="javascript:;">Delete</a>
-                                </Popconfirm>
-                            </span>
-                        )}
-                    />
-                </Table>
-                <UserModal
-                    visible={this.userModalVisible}
-                    onCancel={this.hideUserModalVisible}
-                    user={this.currentUser}
-                />
-            </React.Fragment>
-        )
-    }
-}
-
-export default UserTable
+)(observer(UserTable))
