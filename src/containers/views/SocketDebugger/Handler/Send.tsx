63104bc170fae35f677d3aeedc6dacb9f742b185
shanghaitao <359310444@qq.com>
2019-05-10 21:18:23 +0800

    chore: functional Send

--- src/containers/views/SocketDebugger/Handler/Send.tsx
+++ src/containers/views/SocketDebugger/Handler/Send.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { observer, inject } from 'mobx-react'
+import { inject } from 'mobx-react'
+import { observer, useComputed } from 'mobx-react-lite'
 import { observable, action, toJS, computed } from 'mobx'
 import { Button, AutoComplete, Popconfirm, Modal, Input, message } from 'antd'
 import ReactJson, { InteractionProps } from 'react-json-view'
@@ -12,7 +13,6 @@ import { send } from '@services/websocket'
 interface IStoreProps {
     isSocketIO?: boolean
     socketIsConnected?: boolean
-    setSocketType?: (type: ISocketStore.SocketType) => void
     dataFormat?: ISocketStore.DataFormatType
 }
 
@@ -22,66 +22,36 @@ if (initialSocketIOEvents.length > 30) {
     initialSocketIOEvents = initialSocketIOEvents.slice(0, 30)
 }
 
-@inject(
-    (store: IStore): IStoreProps => {
-        const { socketIsConnected, isSocketIO, setSocketType, dataFormat } = store.socketStore
-        return { socketIsConnected, isSocketIO, setSocketType, dataFormat }
-    }
-)
-@observer
-class Send extends React.Component<IStoreProps> {
-    @observable
-    private content: string = ''
-    @observable
-    private textContent: string = ''
-    @observable.ref
-    private jsonContent: PlainObject = {}
-    @observable
-    private socketIOEvent: string = ''
-    @observable
-    private socketIOEvents: string[] = initialSocketIOEvents
-    @observable
-    private modalVisible: boolean = false
+function Send({ isSocketIO, socketIsConnected, dataFormat }: IStoreProps) {
+    const [content, setContent] = React.useState('')
+    const [textContent, setTextContent] = React.useState('')
+    const [jsonContent, setJsonContent] = React.useState<PlainObject>({})
+    const [socketIOEvent, setSocketIOEvent] = React.useState('')
+    const [socketIOEvents, setSocketIOEvents] = React.useState(initialSocketIOEvents)
+    const [modalVisible, setModalVisible] = React.useState(false)
 
-    @computed
-    get canSend() {
-        const { socketIsConnected, isSocketIO } = this.props
-        if (isSocketIO && !this.socketIOEvent) {
+    const canSend = useComputed(() => {
+        if (isSocketIO && !socketIOEvent) {
             return false
         }
         return socketIsConnected
-    }
+    }, [isSocketIO, socketIOEvent, socketIsConnected])
 
-    @computed
-    get sendingContent() {
-        const { dataFormat } = this.props
+    const sendingContent = useComputed(() => {
         if (dataFormat === DATA_FORMATS[0]) {
-            return this.jsonContent
+            return jsonContent
         }
-        return this.textContent
+        return textContent
+    }, [dataFormat, jsonContent, textContent])
+
+    const toggleModalVisible = () => {
+        setModalVisible(visible => !visible)
     }
 
-    @action
-    toggleModalVisible = () => {
-        this.modalVisible = !this.modalVisible
-    }
-
-    @action
-    handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        this.content = e.target.value
-    }
-
-    @action
-    handleTextContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        this.textContent = e.target.value
-    }
-
-    @action
-    handleOK = () => {
+    const handleOK = () => {
         try {
-            const jsonContent = JSON.parse(this.content)
-            this.jsonContent = jsonContent
-            this.toggleModalVisible()
+            setJsonContent(JSON.parse(content))
+            toggleModalVisible()
         } catch (err) {
             console.error(err)
             message.destroy()
@@ -89,109 +59,95 @@ class Send extends React.Component<IStoreProps> {
         }
     }
 
-    @action
-    handleReactJsonChange = (e: InteractionProps) => {
-        this.jsonContent = e.updated_src
-    }
-
-    @action
-    reset = () => {
-        this.jsonContent = {}
-    }
-
-    @action
-    handleSocketEventChange = (e: string) => {
-        this.socketIOEvent = e
-    }
-
-    @action
-    handleSubmit = () => {
-        if (!this.props.isSocketIO) {
-            return send(null, this.sendingContent)
-        } else if (!this.socketIOEvent) {
+    const handleSubmit = () => {
+        if (!isSocketIO) {
+            return send(null, sendingContent)
+        } else if (!socketIOEvent) {
             message.destroy()
             return message.error('Please input event name!')
         }
-        if (!this.socketIOEvents.includes(this.socketIOEvent)) {
-            this.socketIOEvents.unshift(this.socketIOEvent)
-            localStorage.setItem(LOCALSTORAGE_KEYS.SOCKET_IO_EVENTS, JSON.stringify(this.socketIOEvents))
+        if (!socketIOEvents.includes(socketIOEvent)) {
+            setSocketIOEvents([socketIOEvent, ...socketIOEvents])
+            localStorage.setItem(LOCALSTORAGE_KEYS.SOCKET_IO_EVENTS, JSON.stringify(socketIOEvents))
         }
-        send(this.socketIOEvent, this.sendingContent)
+        send(socketIOEvent, sendingContent)
     }
 
-    render() {
-        const { isSocketIO, dataFormat } = this.props
-        return (
-            <div>
-                {isSocketIO && (
-                    <AutoComplete
-                        className={styles.autoComplete}
-                        dataSource={toJS(this.socketIOEvents)}
-                        placeholder="Input event name"
-                        value={this.socketIOEvent}
-                        onChange={this.handleSocketEventChange}
-                        filterOption={(inputValue, option) =>
-                            (option.props.children as string).toUpperCase().includes(inputValue.toUpperCase())
-                        }
-                    />
-                )}
-                {dataFormat === DATA_FORMATS[0] ? (
-                    <div className={styles.content}>
-                        <div className={styles.reset}>
-                            <Popconfirm placement="topLeft" title="Confirm to reset?" onConfirm={this.reset}>
-                                <Button>Reset</Button>
-                            </Popconfirm>
-                            <Button className={styles.btnCover} type="primary" onClick={this.toggleModalVisible}>
-                                Custom
-                            </Button>
-                            <Modal
-                                title="Custom data"
-                                style={{ top: 20 }}
-                                visible={this.modalVisible}
-                                onOk={this.handleOK}
-                                onCancel={this.toggleModalVisible}
-                            >
-                                <Input.TextArea
-                                    placeholder="Please input json string"
-                                    autosize={{ minRows: 4, maxRows: 10 }}
-                                    value={this.content}
-                                    onChange={this.handleContentChange}
-                                />
-                            </Modal>
-                        </div>
-                        <ReactJson
-                            name={false}
-                            theme="monokai"
-                            style={{
-                                padding: 10,
-                                minHeight: 150,
-                                maxHeight: 500,
-                                overflow: 'auto',
-                                borderRadius: 3,
-                                wordWrap: 'break-word',
-                                wordBreak: 'break-all'
-                            }}
-                            onAdd={this.handleReactJsonChange}
-                            onEdit={this.handleReactJsonChange}
-                            onDelete={this.handleReactJsonChange}
-                            src={this.jsonContent}
-                        />
+    return (
+        <div>
+            {isSocketIO && (
+                <AutoComplete
+                    className={styles.autoComplete}
+                    dataSource={toJS(socketIOEvents)}
+                    placeholder="Input event name"
+                    value={socketIOEvent}
+                    onChange={e => setSocketIOEvent(e as string)}
+                    filterOption={(inputValue, option) =>
+                        (option.props.children as string).toUpperCase().includes(inputValue.toUpperCase())
+                    }
+                />
+            )}
+            {dataFormat === DATA_FORMATS[0] ? (
+                <div className={styles.content}>
+                    <div className={styles.reset}>
+                        <Popconfirm placement="topLeft" title="Confirm to reset?" onConfirm={() => setJsonContent({})}>
+                            <Button>Reset</Button>
+                        </Popconfirm>
+                        <Button className={styles.btnCover} type="primary" onClick={toggleModalVisible}>
+                            Custom
+                        </Button>
+                        <Modal
+                            title="Custom data"
+                            style={{ top: 20 }}
+                            visible={modalVisible}
+                            onOk={handleOK}
+                            onCancel={toggleModalVisible}
+                        >
+                            <Input.TextArea
+                                placeholder="Please input json string"
+                                autosize={{ minRows: 4, maxRows: 10 }}
+                                value={content}
+                                onChange={({ target }) => setContent(target.value)}
+                            />
+                        </Modal>
                     </div>
-                ) : (
-                    <Input.TextArea
-                        className={styles.textContent}
-                        placeholder="Input your sending content"
-                        autosize={{ minRows: 4, maxRows: 10 }}
-                        value={this.textContent}
-                        onChange={this.handleTextContentChange}
+                    <ReactJson
+                        name={false}
+                        theme="monokai"
+                        style={{
+                            padding: 10,
+                            minHeight: 150,
+                            maxHeight: 500,
+                            overflow: 'auto',
+                            borderRadius: 3,
+                            wordWrap: 'break-word',
+                            wordBreak: 'break-all'
+                        }}
+                        onAdd={({ updated_src }) => setJsonContent(updated_src)}
+                        onEdit={({ updated_src }) => setJsonContent(updated_src)}
+                        onDelete={({ updated_src }) => setJsonContent(updated_src)}
+                        src={jsonContent}
                     />
-                )}
-                <Button block size="large" disabled={!this.canSend} type="primary" onClick={this.handleSubmit}>
-                    Send
-                </Button>
-            </div>
-        )
-    }
+                </div>
+            ) : (
+                <Input.TextArea
+                    className={styles.textContent}
+                    placeholder="Input your sending content"
+                    autosize={{ minRows: 4, maxRows: 10 }}
+                    value={textContent}
+                    onChange={({ target }) => setTextContent(target.value)}
+                />
+            )}
+            <Button block size="large" disabled={!canSend} type="primary" onClick={handleSubmit}>
+                Send
+            </Button>
+        </div>
+    )
 }
 
-export default Send
+export default inject(
+    (store: IStore): IStoreProps => {
+        const { socketIsConnected, isSocketIO, dataFormat } = store.socketStore
+        return { socketIsConnected, isSocketIO, dataFormat }
+    }
+)(observer(Send))
