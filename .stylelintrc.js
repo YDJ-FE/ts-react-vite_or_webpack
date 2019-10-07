1451bd2353a84ad71b21d269a7ffbbff1bb62f44
shanghaitao <shanghaitao@qq.com>
2019-10-07 14:23:27 +0800

    chore: stylelint fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,12 @@
 // @ts-nocheck
 module.exports = {
-    extends: ['stylelint-prettier/recommended', 'stylelint-config-ydj/scss', 'stylelint-config-ydj/prettier'],
+    extends: ['stylelint-prettier/recommended'],
+    plugins: ['stylelint-order'],
     rules: {
         'prettier/prettier': true,
         'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'local'] }],
-        'property-no-unknown': [true, { ignoreProperties: ['composes'] }]
+        'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
+        'order/order': ['custom-properties', 'declarations'],
+        'order/properties-alphabetical-order': true
     }
 }
